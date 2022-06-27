20af6320467c6894c96a348f03b4bd6727dba546
topthetop <tuanak691@gmail.com>
2022-06-27 16:18:33 +0700

    @

--- src/routes/site.route.ts
+++ src/routes/site.route.ts
@@ -5,7 +5,7 @@ import express,{Request,Response,NextFunction} from "express";
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.json({ message: "Welcome to PBL7 !!" });
+    res.json({ message: "Welcome to PBL7 @@" });
 });
 
 export default router;
